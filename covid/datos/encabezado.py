1892142d8a36096953d63a6ef5dc9a9eee0e6757
Damian Yukio Romero Diaz <damianyukioromerodiaz@Damians-MacBook-Pro.local>
2020-04-05 03:26:41 -0500

    add vs graphs (totals)

--- covid/datos/encabezado.py
+++ covid/datos/encabezado.py
@@ -12,20 +12,23 @@ def ordenar_fechas(directorio):
     if file.startswith(prefix) and file.endswith(sufix):
       my_dates.append(file.split('_')[1])
   my_dates.sort(key = lambda date: datetime.strptime(date, '%d-%m-%Y'))
-  print(my_dates)
-  return my_dates[-1], prefix+my_dates[0]+sufix
+  return my_dates[-1], prefix+my_dates[-1]+sufix
 
 def printmd(fecha_actualizacion):
+  fecha_hoy = datetime.today().strftime('%d-%m-%Y')
   display(Markdown(f"""# Exploración de datos de COVID19
   ## Actualizado el: {fecha_actualizacion}
   """))
 
-  display(Markdown(f"""### Fecha de hoy: {datetime.today().strftime('%d/%m/%y')}
+  display(Markdown(f"""### Fecha de hoy: {fecha_hoy}
   """))
 
-  display(Markdown(f"""_Si quieres tener la fecha más reciente,
-  lee la documentación de este proyecto:
-  [https://github.com/damian-romero/covid19_mx/blob/master/README.md]_"""))
+  if not fecha_hoy == fecha_actualizacion:
+    display(Markdown(f"""_Si quieres tener la fecha más reciente,
+    lee la documentación de este proyecto:
+    [https://github.com/damian-romero/covid19_mx/blob/master/README.md]_"""))
+  else:
+    display(Markdown(f"""_Los datos fueron actualizados el día de hoy_"""))
 
   display(Markdown(f"""#### Este archivo te permitirá explorar los datos del contagio de COVID19 [https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide]"""))
 
@@ -33,15 +36,8 @@ def printhdr(fecha_actualizacion):
   print(f"""# Exploración de datos de COVID19 :octocat:
   ## Actualizado el: {fecha_actualizacion}
   """)
-
   print(f"""### Fecha de hoy: {datetime.today().strftime('%d/%m/%y')}
   """)
-
-  print(f"""_Si quieres tener la fecha más reciente,
-  lee la documentación de este proyecto:
-  [https://github.com/damian-romero/covid19_mx/blob/master/README.md]_""")
-
-  print(f"""#### Este archivo te permitirá explorar los datos del contagio de COVID19 [https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide]""")
 
 if __name__ == '__main__':
   directorio = '.'
