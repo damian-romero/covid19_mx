307195c1051bb3d46c0c1d9ad70c139db58328bb
Damian Yukio Romero Diaz <damianyukioromerodiaz@Damians-MacBook-Pro.local>
2020-04-04 23:34:17 -0600

    add unit test for encabezado.py

--- covid/datos/encabezado.py
+++ covid/datos/encabezado.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from IPython.display import display, Markdown, Latex
 
 
-def ordenar_fechas():
-  files = os.listdir('./datos')
+def ordenar_fechas(directorio):
+  files = os.listdir(directorio)
   prefix = 'covid19_'
   sufix = '_.csv'
   my_dates = []
@@ -12,22 +12,42 @@ def ordenar_fechas():
     if file.startswith(prefix) and file.endswith(sufix):
       my_dates.append(file.split('_')[1])
   my_dates.sort(key = lambda date: datetime.strptime(date, '%d-%m-%Y'))
-  return my_dates[0], prefix+my_dates[0]+sufix
+  print(my_dates)
+  return my_dates[-1], prefix+my_dates[0]+sufix
 
-fecha_actualizacion, tabla_de_datos = ordenar_fechas()
+def printmd(fecha_actualizacion):
+  display(Markdown(f"""# Exploración de datos de COVID19
+  ## Actualizado el: {fecha_actualizacion}
+  """))
 
+  display(Markdown(f"""### Fecha de hoy: {datetime.today().strftime('%d/%m/%y')}
+  """))
 
-display(Markdown(f"""# Exploración de datos de COVID19 :octocat:
-## Actualizado el: {fecha_actualizacion}
-"""))
+  display(Markdown(f"""_Si quieres tener la fecha más reciente,
+  lee la documentación de este proyecto:
+  [https://github.com/damian-romero/covid19_mx/blob/master/README.md]_"""))
 
-display(Markdown(f"""### Fecha de hoy: {datetime.today().strftime('%d/%m/%y')}
-"""))
+  display(Markdown(f"""#### Este archivo te permitirá explorar los datos del contagio de COVID19 [https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide]"""))
 
-display(Markdown(f"""_Si quieres tener la fecha más reciente,
-lee la documentación de este proyecto:
-[https://github.com/damian-romero/covid19_mx/blob/master/README.md]_"""))
+def printhdr(fecha_actualizacion):
+  print(f"""# Exploración de datos de COVID19 :octocat:
+  ## Actualizado el: {fecha_actualizacion}
+  """)
 
+  print(f"""### Fecha de hoy: {datetime.today().strftime('%d/%m/%y')}
+  """)
 
-display(Markdown(f"""#### Este archivo te permitirá explorar los datos del contagio de COVID19 [https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide]"""))
+  print(f"""_Si quieres tener la fecha más reciente,
+  lee la documentación de este proyecto:
+  [https://github.com/damian-romero/covid19_mx/blob/master/README.md]_""")
 
+  print(f"""#### Este archivo te permitirá explorar los datos del contagio de COVID19 [https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide]""")
+
+if __name__ == '__main__':
+  directorio = '.'
+  fecha_actualizacion, tabla_de_datos = ordenar_fechas(directorio)
+  printhdr(fecha_actualizacion)
+else:
+  directorio = './datos'
+  fecha_actualizacion, tabla_de_datos = ordenar_fechas(directorio)
+  printmd(fecha_actualizacion)
